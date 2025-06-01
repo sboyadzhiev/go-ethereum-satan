6def6d31f85dabdf6b0edb892e33c930b52a9ff9
Stilian Boyadzhiev <stilian.boyadzhiev@gmail.com>
2025-06-01 11:10:53 +0300

    forking other network

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -11,6 +11,18 @@ module.exports = {
       //   // Replace with the private key from Geth dev account
       //   "0xb71c71a67e1177ad4e901695e1b4b9ee17ae16c6668d313eac2f96dbcda3f291"
       // ]
-    }
+    },
+    docker: {
+      url: "http://localhost:8545", // or the appropriate port
+      accounts: [
+        // Use one of the accounts from your dockerized node
+        "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80" // Ganache default key
+      ]
+    },
+    hardhat: {
+      forking: {
+        url: "http://localhost:8545", // Geth node’s RPC
+      }
+    }    
   }
 };
